689101b52635fd767d044d82f164da5d9db77b8a
AdrianKopczynski <115359523+AdrianKopczynski@users.noreply.github.com>
2023-01-08 00:05:50 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,20 +15,16 @@
 #include <cstdlib>
 #include <vector>
 #include <unistd.h>
+#include <sstream>
 
 using namespace std;
 
-/*
-	//zapis najwyzszego uzyskanego wyniku w pliku hScore.txt
-	ofstream hScoreSave("hScore.txt"); 
-	hScoreSave << hScore; 
-	hScoreSave.close();*/
-
 const int defSnakeLength = 2; // Ustanowienie domyœlnej d³ugoœci cia³a wê¿a jako 2 wg specyfikacji zadania
-string hScore,head = "@ ",body = "# ",blank = "  ",mapBorder = "X ",food = "O ";
-int difficulty,sizeOfMap,size,buttonPressed,snakeBodyLength,direction,tableRows,tableColumn,headCordinates,gameSpeed,tempX,tempY,score,minFoodCord,maxFoodCord;
+string hScore,result,head = "@ ",body = "# ",blank = "  ",mapBorder = "X ",food = "O ";
+int difficulty,sizeOfMap,size,buttonPressed,snakeBodyLength,direction,tableRows,tableColumn,headCordinates,gameSpeed,tempX,tempY,score,minFoodCord,maxFoodCord,usrInput,hScoreLast;
 bool lose=false;
 bool isFoodConsumed=false;
+bool resume;
 vector <int> headCords(2);
 vector <int> bodyX(2);
 vector <int> bodyY(2);
@@ -48,14 +44,14 @@ void coolPrint(string string,int x){
 
 void titlePrint(){
 	system("cls");
-	cout << "------------------------------------------------------------------                             HighScore:" << hScore << endl;
+	cout << "------------------------------------------------------------------"<< endl;
 	cout << "    _________    ____     __       ___       __   ___  ________" << endl;
 	cout << "   / ________\\  |    \\   |  |     /   \\     |  | /  / |  ______| " << endl;
 	cout << "  |  |_______   |     \\  |  |    /  _  \\    |  |/  /  |  |____" << endl;	
 	cout << "   \\_______  \\  |  |\\  \\ |  |   /  /_\\  \\   |     /   |  _____|" << endl;
 	cout << "    _______|  | |  | \\  \\|  |  /  _____  \\  |  |\\  \\  |  |_____" << endl;	
 	cout << "   /_________/  |__|  \\_____| /__/     \\__\\ |__| \\__\\ |________|" << endl << endl;
-	cout << "------------------------------------------------------------------" << endl << endl;
+	cout << "------------------------------------------------------------------   HighScore:" << hScore << endl << endl;
 }
 
 void hScoreLoad(){
@@ -122,6 +118,7 @@ void menu(){
 	coolPrint("1. Easy", 10);
 	coolPrint("2. Normal", 10);
 	coolPrint("3. Hard", 10);
+	coolPrint("Press 1, 2 or 3 to select difficulty level.",10);
     difficulty = choice(); // czekamy az uzytkownik wybierze poziom trudnosci, 49-"1" 50-"2" 51-"3"
     titlePrint();
     string selectedDif;
@@ -135,10 +132,11 @@ void menu(){
 		selectedDif = "Selcected difficulty is: Hard.";
 	}
 	coolPrint (selectedDif, 10);
-    coolPrint("Select board size :",10);
+    coolPrint("Select map size :",10);
     coolPrint("1. Small", 10);
 	coolPrint("2. Normal", 10);
 	coolPrint("3. XXL", 10);
+	coolPrint("Press 1, 2 or 3 to select map size.",10);
 	sizeOfMap = choice(); // czekamy az uzytkownik wybierze poziom trudnosci, 49-"1" 50-"2" 51-"3"
     titlePrint();
     string selectedSize;
@@ -154,7 +152,7 @@ void menu(){
 	coolPrint (selectedDif, 10);
 	coolPrint (selectedSize, 10);
 	coolPrint ("Game Starts in ",10);
-	//coolPrint ("3...2...1...0",500);
+	coolPrint ("3...2...1...0",300);
 }
 
 int declareSize(int size){
@@ -195,7 +193,7 @@ void buttonPress(){
 int main(){
 	hScoreLoad();
 	Sleep(100);
-	//welcomeScreen();
+	welcomeScreen();
 	menu();
 	system("cls");
 	size = declareSize(sizeOfMap);
@@ -215,7 +213,7 @@ int main(){
 	}
 	headCordinates = (size-1)/2;
 	minFoodCord=1;
-	maxFoodCord=size-2;
+	maxFoodCord=size-1;
 	map[headCordinates][headCordinates] = head;
 	map[headCordinates+1][headCordinates] = body;
 	map[headCordinates+2][headCordinates] = body;
@@ -228,8 +226,9 @@ int main(){
 	bodyY[1]=headCordinates+2;
 	blankSpace[0]=headCordinates;
 	blankSpace[1]=headCordinates+3;
-	food[0] = 2;
-	food[1] = 2;
+	foodLocation[0] = 2;
+	foodLocation[1] = 2;
+	map[foodLocation[0]][foodLocation[1]] = food;
 	for(int i=0;i<size;i++){
 		for(int j=0;j<size;j++){
 			cout<<map[i][j];
@@ -252,11 +251,17 @@ int main(){
 		system("cls");
 		if(isFoodConsumed==true){
 			//losowe generowanie koordynatów jedzenia gdy poprzednie zosta³o zjedzone 
-			food[0] = (rand() % (minFoodCord + maxFoodCord));
-			food[1] = (rand() % (minFoodCord + maxFoodCord));
-			isFoodConsumed=false;
+			foodLocation[0] = (rand() % (minFoodCord + maxFoodCord));
+			foodLocation[1] = (rand() % (minFoodCord + maxFoodCord));
+			if(map[foodLocation[0]][foodLocation[1]] == mapBorder || map[foodLocation[0]][foodLocation[1]] == head || map[foodLocation[0]][foodLocation[1]] == body ){
+				foodLocation[0] = (rand() % (minFoodCord + maxFoodCord));
+				foodLocation[1] = (rand() % (minFoodCord + maxFoodCord));
+			}
+			else{
+				map[foodLocation[0]][foodLocation[1]] = food;
+				isFoodConsumed=false;
+			}
 		}
-		map[food[0]][food[1]] = food;
 		tempX = bodyX[0];
 		tempY = bodyY[0];
 		bodyX[0] = headCords[1];
@@ -328,16 +333,63 @@ int main(){
 			map[bodyY[i]][bodyX[i]] = body;
 		}
 		map[blankSpace[0]][blankSpace[1]] = blank;
-		
 		for(int i=0;i<size;i++){
 			for(int j=0;j<size;j++){
 				cout<<map[i][j];
 			}
+		if(i==(size-1)/2){
+			cout<< "  Score:" << score;
+		}
 		cout << endl;
 		}
 		Sleep(gameSpeed);
 		buttonPress();
 	}while(lose==false);
-	
-	return 0;
+	cout <<endl<<endl;
+	istringstream(hScore) >> hScoreLast;
+	if(score < hScoreLast){
+		cout << "Your score: " << score << endl;
+		cout << "Highest score: " << hScore << endl;
+	}
+	else{
+		cout << "New highest score!!   " << score << endl;
+		//zapis najwyzszego uzyskanego wyniku w pliku hScore.txt
+		result = score;
+		cout << result;
+		ofstream hScoreSave;
+		hScoreSave.open("hScore.txt");
+		hScoreSave << result; 
+		hScoreSave.close();
+	}
+	cout << "Do you want to play again?(y/n)";
+	while(kbhit()){
+		usrInput = getch(); //y=121, n=110
+	}
+	int done;
+	do
+	{
+    	done = 1;
+    	int c =getch(); 
+    	switch( c )
+    	{
+        	case 121: 
+        	usrInput= 121;
+        	break;
+
+        	case 110: 
+           	usrInput =110;
+            break;
+
+        	default: 
+            done = 0;
+            break;
+    	}
+	} while ( !done );
+	if(usrInput==121){
+		system("cls");
+		main();
+	}
+	else if(usrInput==110){
+		return 0;	
+	}
 }
